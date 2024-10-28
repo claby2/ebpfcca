5e89945c12c534a08375f50b674876373166cd22
vboxuser <vboxuser@vbox.brown.edu>
2024-10-28 13:54:43 -0400

    Performed benchmark on cubic CCA

---
+++ benchmark/benchmark.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# on server side:
+# sudo apt install iperf3
+# iperf3 -s -p {TARGET_PORT}
+
+TARGET_SERVER=bokaibi.com
+TARGET_PORT=5142
+TOTAL_SECONDS=30
+
+# native cubic
+touch cubic.txt
+sudo sysctl --write net.ipv4.tcp_congestion_control=cubic
+iperf3 -c $TARGET_SERVER -p $TARGET_PORT \
+--json --timestamps \
+--time $TOTAL_SECONDS --logfile cubic.txt
+
+sleep 5
+
+# bpf cubic
+touch bpf_cubic.txt
+sudo sysctl --write net.ipv4.tcp_congestion_control=bpf_cubic
+iperf3 -c $TARGET_SERVER -p $TARGET_PORT \
+--json --timestamps \
+--time $TOTAL_SECONDS --logfile bpf_cubic.txt
