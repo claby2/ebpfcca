998b5a8e70baf9871ace1d7df2ddd2aa91b0c691
Edward Wibowo <wibow9770@gmail.com>
2024-11-01 10:51:31 -0400

    feat: send bytes_acked signal to user-space

--- ebpfccp/src/bpf/datapath.bpf.c
+++ ebpfccp/src/bpf/datapath.bpf.c
@@ -1,12 +1,7 @@
-#include "bpf_cubic.h"
+#include "datapath.bpf.h"
+#include "vmlinux.h"
+#include <bpf/bpf_core_read.h>
 #include <bpf/bpf_tracing.h>
-
-struct signal {
-  u64 now;
-};
-
-// dummy signal, this is needed so user-land can access the struct information
-struct signal _signal = {0};
 
 // Ring buffer to send signal events to user-land
 struct {
@@ -14,31 +9,10 @@ struct {
   __uint(max_entries, sizeof(struct signal) * 1024);
 } signals SEC(".maps");
 
-SEC("struct_ops")
-void BPF_PROG(init, struct sock *sk) { bpf_cubic_init(sk); }
+void load_signal(struct sock *sk, const struct rate_sample *rs) {
+  struct tcp_sock *tp = tcp_sk(sk);
+  struct ccp *ca = inet_csk_ca(sk);
 
-SEC("struct_ops")
-void BPF_PROG(cwnd_event, struct sock *sk, enum tcp_ca_event event) {
-  bpf_cubic_cwnd_event(sk, event);
-}
-
-SEC("struct_ops")
-void BPF_PROG(cong_avoid, struct sock *sk, __u32 ack, __u32 acked) {
-  bpf_cubic_cong_avoid(sk, ack, acked);
-}
-
-SEC("struct_ops")
-__u32 BPF_PROG(ssthresh, struct sock *sk) {
-  return bpf_cubic_recalc_ssthresh(sk);
-}
-
-SEC("struct_ops")
-void BPF_PROG(set_state, struct sock *sk, __u8 new_state) {
-  bpf_cubic_state(sk, new_state);
-}
-
-SEC("struct_ops")
-void BPF_PROG(pckts_acked, struct sock *sk, const struct ack_sample *sample) {
   struct signal *sig;
 
   // Reserve bytes in the ring buffer and get a pointer to the reserved space
@@ -46,35 +20,58 @@ void BPF_PROG(pckts_acked, struct sock *sk, const struct ack_sample *sample) {
   if (!sig)
     return;
 
-  sig->now = bpf_ktime_get_ns();
+  u64 rin = 0;  // send bandwidth in bytes per second
+  u64 rout = 0; // recv bandwidth in bytes per second
+  u64 ack_us = rs->rcv_interval_us;
+  u64 snd_us = rs->snd_interval_us;
+
+  if (ack_us != 0 && snd_us != 0) {
+    rin = rout = (u64)rs->delivered * MTU * S_TO_US;
+    do_div(rin, snd_us);
+    do_div(rout, ack_us);
+  }
+
+  sig->bytes_acked = tp->bytes_acked - ca->last_bytes_acked;
+  ca->last_bytes_acked = tp->bytes_acked;
 
   // Submit the reserved space to the ring buffer
   bpf_ringbuf_submit(sig, 0);
-
-  // Call the original function... for now, cubic just runs in the background
-  bpf_cubic_acked(sk, sample);
 }
 
 SEC("struct_ops")
-__u32 BPF_PROG(undo_cwnd, struct sock *sk) { return bpf_cubic_undo_cwnd(sk); }
+void BPF_PROG(init, struct sock *sk) { return; }
+
+SEC("struct_ops")
+void BPF_PROG(cwnd_event, struct sock *sk, enum tcp_ca_event event) { return; }
+
+SEC("struct_ops")
+void BPF_PROG(cong_control, struct sock *sk, const struct rate_sample *rs) {
+  load_signal(sk, rs);
+}
+
+SEC("struct_ops")
+__u32 BPF_PROG(ssthresh, struct sock *sk) { return 0; }
+
+SEC("struct_ops")
+void BPF_PROG(set_state, struct sock *sk, __u8 new_state) { return; }
+
+SEC("struct_ops")
+void BPF_PROG(pckts_acked, struct sock *sk, const struct ack_sample *sample) {
+  return;
+}
+
+SEC("struct_ops")
+__u32 BPF_PROG(undo_cwnd, struct sock *sk) { return 0; }
 
 SEC(".struct_ops")
 struct tcp_congestion_ops ebpfccp = {
-    // initialize private data (optional)
     .init = (void *)init,
-    // return slow start threshold (required)
     .ssthresh = (void *)ssthresh,
-    // do new cwnd calculation (required)
-    .cong_avoid = (void *)cong_avoid,
-    // call before changing ca_state (optional)
+    .cong_control = (void *)cong_control,
     .set_state = (void *)set_state,
-    // new value of cwnd after loss (optional)
     .undo_cwnd = (void *)undo_cwnd,
-    // call when cwnd event occurs (optional)
     .cwnd_event = (void *)cwnd_event,
-    // hook for packet ack accounting (optional)
     .pkts_acked = (void *)pckts_acked,
-    // get info for inet_diag (optional)
     .get_info = NULL,
     .release = NULL,
     .name = "ebpfccp",
---
+++ ebpfccp/src/bpf/datapath.bpf.h
@@ -0,0 +1,55 @@
+#ifndef DATAPATH_BPF_H
+#define DATAPATH_BPF_H
+
+#include "vmlinux.h"
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_tracing.h>
+
+char _license[] SEC("license") = "GPL";
+
+struct signal {
+  // newly acked, in-order bytes
+  u32 bytes_acked;
+  // TODO: Add more congestion primitives
+  //       See `ccp_primitives` in ccp-project/libccp cpp.h
+  //       for more information
+};
+
+// dummy signal, this is needed so user-land can access the struct information
+struct signal _signal = {0};
+
+// This represents the as the per-socket private data
+// It can be accessed with inet_csk_ca(sk)
+struct ccp {
+  // control
+  u32 last_snd_una;     // 4 B
+  u32 last_bytes_acked; // 8 B
+  u32 last_sacked_out;  // 12 B
+};
+
+static inline struct inet_connection_sock *inet_csk(const struct sock *sk) {
+  return (struct inet_connection_sock *)sk;
+}
+
+static inline void *inet_csk_ca(const struct sock *sk) {
+  return (void *)inet_csk(sk)->icsk_ca_priv;
+}
+
+static inline struct tcp_sock *tcp_sk(const struct sock *sk) {
+  return (struct tcp_sock *)sk;
+}
+
+#define MTU 1500
+#define S_TO_US 1000000
+
+// Copied this from the kernel source code
+#define do_div(n, base)                                                        \
+  ({                                                                           \
+    uint32_t __base = (base);                                                  \
+    uint32_t __rem;                                                            \
+    __rem = ((uint64_t)(n)) % __base;                                          \
+    (n) = ((uint64_t)(n)) / __base;                                            \
+    __rem;                                                                     \
+  })
+
+#endif
