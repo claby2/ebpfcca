f83d89c5eafdd015c0306e5af4b2a04b62a582c1
Bokai-Bi <bokai.bi0@gmail.com>
2024-11-17 16:46:05 -0500

    feat: Modularized benchmark script and allowed for multiple trials

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -4,23 +4,23 @@
 # sudo apt install iperf3
 # iperf3 -s -p {TARGET_PORT}
 
-TARGET_SERVER=bokaibi.com
-TARGET_PORT=5142
-TOTAL_SECONDS=30
-REPORT_INTERVAL=0.1
+TARGET_SERVER=$1
+TARGET_PORT=$2
+TOTAL_SECONDS=$3
+REPORT_INTERVAL=$4
+CCA=$5
+TRIALS=$6
 
-# native cubic
-touch cubic.json
-sudo sysctl --write net.ipv4.tcp_congestion_control=cubic
-iperf3 -c $TARGET_SERVER -p $TARGET_PORT \
---json --verbose --timestamps --interval $REPORT_INTERVAL \
---time $TOTAL_SECONDS --logfile cubic.json
+# loop for trials
+for i in $(seq 1 $TRIALS)
+do
+    echo "Trial $i for $CCA Starting..."
+    touch $CCA_$i.json
+    sudo sysctl --write net.ipv4.tcp_congestion_control=$CCA
+    iperf3 -c $TARGET_SERVER -p $TARGET_PORT \
+    --json --verbose --timestamps --interval $REPORT_INTERVAL \
+    --time $TOTAL_SECONDS --logfile $CCA_$i.json
 
-sleep 5
-
-# bpf cubic
-touch bpf_cubic.json
-sudo sysctl --write net.ipv4.tcp_congestion_control=bpf_cubic
-iperf3 -c $TARGET_SERVER -p $TARGET_PORT \
---json --verbose --timestamps --interval $REPORT_INTERVAL \
---time $TOTAL_SECONDS --logfile bpf_cubic.json
+    echo "Trial $i for $CCA done, sleeping for 5 seconds"
+    sleep 5
+done
