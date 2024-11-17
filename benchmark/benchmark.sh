6330ff6cd59edc3415cfbb47949ce331808c132e
Bokai-Bi <bokai.bi0@gmail.com>
2024-11-17 17:09:03 -0500

    fix: fixed bug regarding file creation in benchmark.sh

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -10,16 +10,16 @@ TOTAL_SECONDS=$3
 REPORT_INTERVAL=$4
 CCA=$5
 TRIALS=$6
-
+echo $0 $1 $2 $3 $4 $5 $6
 # loop for trials
 for i in $(seq 1 $TRIALS)
 do
     echo "Trial $i for $CCA Starting..."
-    touch $CCA_$i.json
+    touch "${CCA}_${i}.json"
     sudo sysctl --write net.ipv4.tcp_congestion_control=$CCA
     iperf3 -c $TARGET_SERVER -p $TARGET_PORT \
     --json --verbose --timestamps --interval $REPORT_INTERVAL \
-    --time $TOTAL_SECONDS --logfile $CCA_$i.json
+    --time $TOTAL_SECONDS --logfile "${CCA}_${i}.json"
 
     echo "Trial $i for $CCA done, sleeping for 5 seconds"
     sleep 5
